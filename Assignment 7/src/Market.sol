cefe1332ad0fc7dad9ec2af37b958a7ccfd952d7
Thomas <denhollander.thomas@gmail.com>
2022-06-02 16:06:26 +0200

    Update accoding to suggestions

--- Assignment 7/src/Market.sol
+++ Assignment 7/src/Market.sol
@@ -7,6 +7,11 @@ import "yield-utils-v2/token/IERC20.sol";
 
 using TransferHelper for IERC20;
 
+/// @notice Reverted with this error if the contract should be initialized but
+///     wasn't.
+error Uninitialized();
+
+
 /// @title An Automated Market Maker contract.
 /// @notice Provides capability to supply liquidity and to exchange tokens.
 ///     A nice property of AMM's is that liquidity never runs out by selling
@@ -14,34 +19,40 @@ using TransferHelper for IERC20;
 /// @dev Any function of this contract rounds in favour of the contract. It may
 ///     be possible to profit from this contract, for example by holding
 ///     liquidity tokens while these rounding errors accumulate.
+///
+///     For derivations, we use x0 and y0 as the starting supply of `tokenX`
+///     and `tokenY` and x1 and y1 as the end supply of `tokenX` and `tokenY`.
+///     Similarly, z0 and z1 are used to denote the liquidity tokens at the
+///     start and end of the call. The products x0 * y0 and x1 * y1 are called
+///     k0 and k1, respectively. k1 >= k0 after exchanges and should only
+///     increase due to rounding.
 contract Market is ERC20("MarketToken", "MART", 18) {
-
     /// @notice Emitted when the market gets initialized. This can happen
     ///     multiple times if the contracts liquidity gets emptied entirely!
-    event Initialized(address indexed to, uint256 amount1, uint256 amount2);
+    event Initialized(address indexed to, uint256 xAmount, uint256 yAmount);
     /// @notice Emitted when the market receives liquidity while it is already
     ///     initialized.
     event Minted(
         address indexed to,
-        uint256 amount1,
-        uint256 amount2,
+        uint256 xAmount,
+        uint256 yAmount,
         uint256 shares
     );
     event Burned(
         address indexed to,
-        uint256 amount1,
-        uint256 amount2,
+        uint256 xAmount,
+        uint256 yAmount,
         uint256 shares
     );
     event SoldX(
         address indexed account,
-        uint256 amount1,
-        uint256 amount2
+        uint256 xAmount,
+        uint256 yAmount
     );
     event SoldY(
         address indexed account,
-        uint256 amount1,
-        uint256 amount2
+        uint256 xAmount,
+        uint256 yAmount
     );
 
     /// @notice The first token that can be exchanged with the other,
@@ -62,9 +73,9 @@ contract Market is ERC20("MarketToken", "MART", 18) {
     }
 
     /// @notice Initialize this contract by supplying initial liquidity. This
-    ///     will also determine the intial price: `x` of token X and `y` of
-    ///     token Y should have equal value. This function can only be called
-    ///     if there is no liquidity.
+    ///     will also determine the initial price: Supplying `x` of token X and
+    ///     `y` of token Y will cause this contract to have an initial price of
+    ///     `x / y`. This function can only be called if there is no liquidity.
     ///
     ///     The caller should have given sufficient allowance to the contract.
     ///
@@ -74,6 +85,8 @@ contract Market is ERC20("MarketToken", "MART", 18) {
     ///     to rounding.)
     /// @param x The amount of tokens of `tokenX`.
     /// @param y The amount of tokens of `tokenY`.
+    /// @dev Here, liquidity tokens are given out as
+    ///     z = x * y
     function initialize(uint256 x, uint256 y) external returns (uint256 z) {
         require(_totalSupply == 0);
 
@@ -83,7 +96,7 @@ contract Market is ERC20("MarketToken", "MART", 18) {
         tokenX.safeTransferFrom(msg.sender, address(this), x);
         tokenY.safeTransferFrom(msg.sender, address(this), y);
 
-        emit Minted(msg.sender, x, y, z);
+        emit Initialized(msg.sender, x, y);
     }
 
     /// @notice Mint liquidity tokens in proportion to the current price in the
@@ -93,53 +106,52 @@ contract Market is ERC20("MarketToken", "MART", 18) {
     /// @param x The amount of tokenX to supply.
     /// @param y The amount of tokenY to supply.
     /// @return z The amount of liquidity tokens returned.
-    /// @dev The amount of subtracted x and y tokens is computed by setting the
-    ///     values of both equal to that with lesser value. The amount of
-    ///     returned `z` tokens is rounded down from the correct value.
+    /// @dev Here, we have the invariant that `x0 / y0 = x / y`, interpreted as
+    ///     the current price. If the user supplied too much of `x` or `y`, we
+    ///     want to lower the other value and give out less liquidity tokens
+    ///     accordingly.
+    ///
+    ///     We should have
+    ///         x0 * y = y0 * x
+    ///     so, if the left side (aY) is bigger, decrease y and if the right
+    ///     side (aX) is bigger, decrease x. Take the other value as the true
+    ///     value to derive from.
+    ///
+    ///     By how much? Well, divide the other side by x0 or y0. Do either
+    ///         y <- (y0 * x) / x0
+    ///         x <- (x0 * y) / y0
+    ///     to correct the bigger value between x and y. Due to rounding the new
+    ///     value may be slightly smaller and never bigger than the real value.
+    ///
+    ///     Now we need to give out liquidity tokens in proportion to the new
+    ///     liquidity. We need
+    ///         z / z0 = x / x0 = y / y0
+    ///     Compute z from the smaller of `x` and `y`, which we know was the
+    ///     one just recomputed.
+    ///         z <- z0 * x / x0
+    ///         z <- z0 * y / y0
+    ///     Like before, we know that by rounding down we can never give too
+    ///     many liquidity tokens.
     function mint(uint256 x, uint256 y) external returns (uint256 z) {
-        require(_totalSupply > 0);
-
-        // The user should supply x and y in the same ratio as is currenly in
-        // the contract. That means
-        //      x / y = x0 / y0
-        // or,
-        //      x * y0 = y * x0
-        // Shrink x or y to make the equation equal on both sides. Call the
-        // left side z_x and the right side zY.
-
-        // The current supply is given by
-        //      z0 = x0 * y0
-        // The new amount of tokens should be proportional to
-        //      x / x0 = y / y0
-        // So,
-        //      z = z0 * (x / x0) = z0 * (y / y0)
-        //          = x0 * y
-        //          = y0 * x
-
         uint256 x0 = tokenX.balanceOf(address(this));
         uint256 y0 = tokenY.balanceOf(address(this));
 
-        uint256 zX = x * y0;
-        uint256 zY = y * x0;
+        // Require that both balances are non-zero. For a little more gas,
+        // `totalSupply > 0` would be a roughly equivalent check. The advantage
+        // is that it could be performed at the top of the function.
+        if (x0 | y0 == 0) {
+            revert Uninitialized();
+        }
 
-        if (zX > zY) {
-            x = zY / y0;
-            unchecked {
-                // Safe as we have just obtained x via division.
-                // Can be slightly smaller than z_y, due to rounding.
-                // Compute to ensure we don't give out too many liquidity tokens.
-                // We have z <= zY < zX
-                z = x * y0;
-            }
+        uint256 aX = x * y0;
+        uint256 aY = y * x0;
+
+        if (aX > aY) {
+            x = aY / y0;
+            z = _totalSupply * x / x0;
         } else {
-            y = zX / x0;
-            unchecked {
-                // Safe as we have just obtained y via division.
-                // Can be slightly smaller than z_x, due to rounding.
-                // Compute to ensure we don't give out too many liquidity tokens.
-                // We have z <= zX <= zY
-                z = y * x0;
-            }
+            y = aX / x0;
+            z = _totalSupply * y / y0;
         }
 
         _mint(msg.sender, z);
@@ -151,17 +163,30 @@ contract Market is ERC20("MarketToken", "MART", 18) {
 
     /// @notice Burn liquidity tokens in exchange for tokenX and tokenY in
     ///     proportion to the current supply in the contract.
+    ///
+    ///     Fails if the contract is unitialized.
     /// @param z The amount of liquidity tokens to burn.
     /// @return x The amount of tokenX that was sent to the account.
     /// @return y The amount of tokenY that was send to the account.
-    /// @dev The tokens X and Y that are returned to the owner are rounded down
-    ///     from the "real" value.
+    /// @dev We have the invariant that
+    ///         x / x0 = y / y0 = z / z0
+    ///     if we ignore rounding.
+    ///
+    ///     Compute by rewriting the above equation directly:
+    ///         x <- x0 * z / z0
+    ///         y <- y0 * z / z0
+    ///     Rounding means we can never give out too much of the underlying
+    ///     supply.
     function burn(uint256 z) external returns (uint256 x, uint256 y) {
-        uint256 x0 = tokenX.balanceOf(address(this));
-        // x = x0 * (z / z0)
-        x = (x0 * z) / _totalSupply;
-        // y = y0 * (z / z0) = y0 * z / (x0 * y0) = z / x0
-        y = z / x0;
+        uint256 tSup = _totalSupply;
+        // We don't strictly have to check this: division by zero in the
+        // following lines will revert anyway. However, for UX revert with the
+        // correct message. The overhead is 16 gas.
+        if (tSup == 0) {
+            revert Uninitialized();
+        }
+        x = (tokenX.balanceOf(address(this)) * z) / tSup;
+        y = (tokenY.balanceOf(address(this)) * z) / tSup;
 
         _burn(msg.sender, z);
         tokenX.safeTransfer(msg.sender, x);
@@ -176,19 +201,36 @@ contract Market is ERC20("MarketToken", "MART", 18) {
     ///     approval for this contract to send itself tokenX.
     /// @param x The amount of tokenX to sell.
     /// @return y The amount of tokenY that has been transferred.
-    /// @dev Due to rounding, the amount of returned `y` tokens can only be
-    ///     smaller than the "real" value.
+    /// @dev We have the invariant that
+    ///         x0 * y0 <= x1 * y1
+    ///     with the inequality only occuring due to rounding.
+    ///
+    ///     By definition
+    ///         x1 = x0 + x
+    ///         y1 = y0 - y
+    ///     and so
+    ///         x0 * y0 = (x0 + x) * (y0 - y)
+    ///         y0 - y = (x0 * y0) / (x0 + x)
+    ///         -y = -y0 + (x0 * y0) / (x0 + x)
+    ///         y = y0 - (x0 * y0) / (x0 + x)
+    ///           = (y0 * (x0 + x) - x0 * y0) / (x0 + x)
+    ///           = (y0 * x0 + y0 * x - x0 * y0) / (x0 + x)
+    ///           = (y0 * x) / (x0 + x)
+    ///
+    ///     By using integer division, `y` can never be bigger than the real
+    ///     value.
+    ///
+    ///     We don't really need to check if the contract has been initialized.
+    ///     If x0 = 0, then the value of tokenX is infinite and so the user
+    ///     gets the entire supply y0 (which should be close to 0). If y0 = 0,
+    ///     then the value of tokenY is infinite and so the user gets no
+    ///     tokens, as they should. So, even an unitialized contract gives out
+    ///     tokens at the "correct", albeit extreme price.
     function sellX(uint256 x) external returns (uint256 y) {
-        require(_totalSupply > 0);
-
-        // Here, we need
-        //      x0 * y0 = (x0 + x) * (y0 - y)
-        //      y = y0 - x0 * y0 / (x0 + x)
-        //        = y0 * x / (x0 + x)
-        //        = z0 * x / (x0 (x0 + x))
-
         uint256 x0 = tokenX.balanceOf(address(this));
-        y = _totalSupply * x / (x0 * (x0 + x));
+        uint256 y0 = tokenY.balanceOf(address(this));
+
+        y = (y0 * x) / (x0 + x);
 
         tokenX.safeTransferFrom(msg.sender, address(this), x);
         tokenY.transfer(msg.sender, y);
@@ -202,13 +244,13 @@ contract Market is ERC20("MarketToken", "MART", 18) {
     ///     approval for this contract to send itself tokenY.
     /// @param y The amount of tokenY to sell.
     /// @return x The amount of tokenX that has been transferred.
-    /// @dev Due to rounding, the amount of returned `x` tokens can only be
-    ///     smaller than the "real" value.
-    function sellY(uint256 y) external returns (uint256 x) {
-        require(_totalSupply > 0);
-        
+    /// @dev See `sellX` for a derivation of the used equation. That function
+    ///     is entirely symmetric over x <-> y.
+    function sellY(uint256 y) external returns (uint256 x) {        
         uint256 y0 = tokenY.balanceOf(address(this));
-        x = _totalSupply * y / (y0 * (y0 + y));
+        uint256 x0 = tokenX.balanceOf(address(this));
+
+        x = x0 * y / (y0 + y);
 
         tokenY.safeTransferFrom(msg.sender, address(this), y);
         tokenX.transfer(msg.sender, x);
--- Assignment 7/test/Market.t.sol
+++ Assignment 7/test/Market.t.sol
@@ -7,6 +7,25 @@ import "yield-utils-v2/mocks/ERC20Mock.sol";
 import "src/Market.sol";
 
 abstract contract ZeroState is Test {
+    ////////////////////////////
+    // Events from Market.sol //
+    event Initialized(address indexed to, uint256 xAmount, uint256 yAmount);
+    event Minted(
+        address indexed to,
+        uint256 xAmount,
+        uint256 yAmount,
+        uint256 shares
+    );
+    event Burned(
+        address indexed to,
+        uint256 xAmount,
+        uint256 yAmount,
+        uint256 shares
+    );
+    event SoldX(address indexed account, uint256 xAmount, uint256 yAmount);
+    event SoldY(address indexed account, uint256 xAmount, uint256 yAmount);
+    ////////////////////////////
+
     Market market;
     ERC20Mock tokenX;
     ERC20Mock tokenY;
@@ -19,7 +38,7 @@ abstract contract ZeroState is Test {
     address user3 = address(3);
 
     function setUp() public virtual {
-        tokenX = new ERC20Mock("Token Y", "X");
+        tokenX = new ERC20Mock("Token X", "X");
         tokenY = new ERC20Mock("Token Y", "Y");
         market = new Market(tokenX, tokenY);
 
@@ -27,13 +46,20 @@ abstract contract ZeroState is Test {
         tokenX.mint(user1, tokenBalanceX);
         tokenY.mint(user1, tokenBalanceY);
     }
+
+    /// @notice Compute the invariant of the product of the token supplies.
+    function computeK() internal view returns (uint256) {
+        return
+            tokenX.balanceOf(address(market)) *
+            tokenY.balanceOf(address(market));
+    }
 }
 
 abstract contract DepositedState is ZeroState {
     uint256 depositedX = 10**15;
     uint256 depositedY = (2 * depositedX) / 3;
 
-    function setUp() public override {
+    function setUp() public virtual override {
         super.setUp();
 
         vm.startPrank(user1);
@@ -44,21 +70,78 @@ abstract contract DepositedState is ZeroState {
     }
 }
 
-contract ContractTest is ZeroState {
-    function testInitialize() public {
-        uint256 inputX = 10**15;
-        uint256 inputY = 2 * inputX;
+/// @dev Mint more tokens, so that the invariant is not equal to the total
+///     supply of liquidity tokens. The price however, should still be
+///     identical.
+abstract contract MintedState is DepositedState {
+    uint256 mintedX = 24**15;
+    uint256 mintedY = (2 * mintedX) / 3;
 
-        // Initialize by adding liquidity with the two tokens.
+    function setUp() public override {
+        super.setUp();
+
+        vm.startPrank(user1);
+        tokenX.mint(user1, mintedX);
+        tokenX.approve(address(market), mintedX);
+        tokenY.mint(user1, mintedY);
+        tokenY.approve(address(market), mintedY);
+        market.mint(mintedX, mintedY);
+        vm.stopPrank();
+    }
+}
+
+contract ContractTest is ZeroState {
+    /// @notice Test initialization using fuzzing
+    function testInitialize(uint256 inputX, uint256 inputY) public {
+        vm.assume(inputX < tokenBalanceX);
+        vm.assume(inputY < tokenBalanceY);
+
         vm.startPrank(user1);
         tokenX.approve(address(market), inputX);
         tokenY.approve(address(market), inputY);
+
+        vm.expectEmit(true, true, true, true);
+        emit Initialized(user1, inputX, inputY);
+
+        // Initialize by adding liquidity with the two tokens.
         market.initialize(inputX, inputY);
+
         vm.stopPrank();
 
         assertEq(market.balanceOf(user1), inputX * inputY);
         assertEq(tokenX.balanceOf(address(market)), inputX);
         assertEq(tokenY.balanceOf(address(market)), inputY);
+    }
+
+    /// @dev Test a specific input
+    function testInitialize1() public {
+        uint256 inputX = 10**15;
+        uint256 inputY = 2 * inputX;
+        testInitialize(inputX, inputY);
+    }
+
+    /// @dev Test that tokens can't be minted without liquidity.
+    function testMintFailOnUninitialized() public {
+        uint256 inputX = 10**15;
+        uint256 inputY = 2 * inputX;
+
+        vm.startPrank(user1);
+
+        // Approve token transfers
+        tokenX.approve(address(market), inputX);
+        tokenY.approve(address(market), inputY);
+
+        // Mint, we expect an error message
+        vm.expectRevert(Uninitialized.selector);
+        market.mint(inputX, inputY);
+
+        vm.stopPrank();
+    }
+
+    function testBurnFailOnUninitialized(uint256 z) public {
+        // We expect an error message
+        vm.expectRevert(Uninitialized.selector);
+        market.burn(z);
     }
 }
 
@@ -66,25 +149,29 @@ contract DepositedStateTest is DepositedState {
     /// @dev Test whether selling doesn't give the user too much. Due to
     ///     rounding errors exact comparison is not really possible while
     ///     using fuzzing.
-    function testSellX(uint256 x) public {
+    ///
+    ///     Returns the returned `y` from `sellX` for later reuse.
+    function testSellX(uint256 x) public returns (uint256 y) {
         vm.assume(x <= 10**40);
 
+        // First, mint the tokens to sell
         tokenX.mint(user2, x);
 
-        uint256 z = market.totalSupply();
+        // Compute the invariant for later
+        uint256 k = computeK();
 
         vm.startPrank(user2);
+        // Approve transfer
         tokenX.approve(address(market), x);
-        market.sellX(x);
+        // Sell. We don't know the price yet in general so don't check returned
+        // y.
+        vm.expectEmit(true, true, false, false);
+        emit SoldX(user2, x, 0);
+        y = market.sellX(x);
         vm.stopPrank();
 
-        // Test that the market liquidity is enough to cover all liquidity
-        // tokens.
-        assert(
-            tokenX.balanceOf(address(market)) *
-                tokenY.balanceOf(address(market)) >=
-                z
-        );
+        // Check invariant
+        assert(computeK() >= k);
     }
 
     /// @dev Here, test selling in a specific case. Here we know the rounding
@@ -97,25 +184,17 @@ contract DepositedStateTest is DepositedState {
 
         tokenX.mint(user2, x);
 
-        uint256 z = market.totalSupply();
+        uint256 k = computeK();
 
         vm.startPrank(user2);
         tokenX.approve(address(market), x);
+        vm.expectEmit(true, true, true, true);
+        emit SoldX(user2, x, y);
         uint256 yReal = market.sellX(x);
         vm.stopPrank();
 
-        // Test liquidity. The product of the balances should be at least as
-        // much as the liquidity tokens.
-        //
-        // We have small increase of
-        //      z_0 = 666666666666666000000000000000
-        // to
-        //      z_1 = 666666666666666666666666000000
-        assertEq(
-            tokenX.balanceOf(address(market)) *
-                tokenY.balanceOf(address(market)),
-            z + 666666666000000
-        );
+        // k only increases
+        assertEq(computeK(), k + 666666666000000);
 
         // Test that the tokens were transferred correctly.
         assertEq(tokenX.balanceOf(user2), 0);
@@ -132,22 +211,17 @@ contract DepositedStateTest is DepositedState {
 
         tokenX.mint(user2, x);
 
-        uint256 z = market.totalSupply();
+        uint256 k = computeK();
 
         vm.startPrank(user2);
         tokenX.approve(address(market), x);
+        vm.expectEmit(true, true, true, true);
+        emit SoldX(user2, x, y);
         uint256 yReal = market.sellX(x);
         vm.stopPrank();
 
-        // A small increase of
-        //      z_0 = 666666666666666000000000000000
-        // to
-        //      z_1 = 666666666666666666666665651496
-        assertEq(
-            tokenX.balanceOf(address(market)) *
-                tokenY.balanceOf(address(market)),
-            z + 666666665651496
-        );
+        // k only increases
+        assertEq(computeK(), k + 666666665651496);
         assertEq(tokenX.balanceOf(user2), 0);
         assertEq(tokenY.balanceOf(user2), y);
         assertEq(yReal, y);
@@ -155,23 +229,25 @@ contract DepositedStateTest is DepositedState {
 
     /// @dev Test whether selling doesn't give the user too much. Due to
     ///     rounding errors exact comparison is not really possible.
-    function testSellY(uint256 y) public {
+    function testSellY(uint256 y) public returns (uint256 x) {
         vm.assume(y <= 10**40);
 
+        // Mint tokens
         tokenY.mint(user2, y);
 
-        uint256 z = market.totalSupply();
+        // Compute invariant for later
+        uint256 k = computeK();
 
         vm.startPrank(user2);
+        // Approve
         tokenY.approve(address(market), y);
-        market.sellY(y);
+        // Sell. Don't check for yet unknown `y`.
+        vm.expectEmit(true, true, false, false);
+        emit SoldY(user2, x, 0);
+        x = market.sellY(y);
         vm.stopPrank();
 
-        assert(
-            tokenX.balanceOf(address(market)) *
-                tokenY.balanceOf(address(market)) >=
-                z
-        );
+        assert(computeK() >= k);
     }
 
     /// @dev Test whether selling doesn't give the user too much. Due to
@@ -182,18 +258,17 @@ contract DepositedStateTest is DepositedState {
 
         tokenY.mint(user2, y);
 
-        uint256 z = market.totalSupply();
+        uint256 k = computeK();
 
         vm.startPrank(user2);
         tokenY.approve(address(market), y);
+        vm.expectEmit(true, true, true, true);
+        emit SoldY(user2, x, y);
         uint256 xReal = market.sellY(y);
         vm.stopPrank();
 
-        assertEq(
-            tokenX.balanceOf(address(market)) *
-                tokenY.balanceOf(address(market)),
-            z + 666666665168666
-        );
+        // k only increases
+        assertEq(computeK(), k + 666666665168666);
         assertEq(tokenX.balanceOf(user2), x);
         assertEq(tokenY.balanceOf(user2), 0);
         assertEq(xReal, x);
@@ -206,15 +281,68 @@ contract DepositedStateTest is DepositedState {
         uint256 x = 150_000_000;
         uint256 y = 100_000_000;
 
+        uint256 expectedX = x;
+        uint256 expectedY = y - 1;
+        uint256 expectedZ = 99_999_999_000_000_000_000_000;
+
+        uint256 startBalanceX = tokenX.balanceOf(user1);
+        uint256 startBalanceY = tokenY.balanceOf(user1);
+        uint256 startBalanceZ = market.balanceOf(user1);
+
         vm.startPrank(user1);
         tokenX.approve(address(market), x);
         tokenY.approve(address(market), y);
+
+        vm.expectEmit(true, true, true, true);
+        emit Minted(user1, expectedX, expectedY, expectedZ);
+
         uint256 z = market.mint(x, y);
         vm.stopPrank();
 
-        // The tokens should have been subtracted
-        assertEq(tokenX.balanceOf(user1), tokenBalanceX - depositedX - x);
-        assertEq(tokenY.balanceOf(user1), tokenBalanceY - depositedY - y + 1);
+        // The tokens should have been subtracted. Liquidity tokens should have
+        // been added.
+        assertEq(tokenX.balanceOf(user1), startBalanceX - expectedX);
+        assertEq(tokenY.balanceOf(user1), startBalanceY - expectedY);
+        assertEq(market.balanceOf(user1), startBalanceZ + expectedZ);
+
+        // The liquidity tokens issued should be proportional to the tokens
+        // supplied.
+        uint256 x1 = tokenX.balanceOf(address(market));
+        uint256 y1 = tokenX.balanceOf(address(market));
+        uint256 z1 = market.totalSupply();
+        assertEq(z / z1, x / x1);
+        assertEq(z / z1, y / y1);
+    }
+
+    /// @dev Supply tokens in a different ratio to the current price and see if
+    ///     they are subtracted correctly.
+    function testMintDifferentRatio() public {
+        uint256 x = 10**13;
+        uint256 y = 10**15;
+
+        uint256 expectedX = x;
+        uint256 expectedY = (x * 2) / 3;
+        uint256 expectedZ = 6_666_666_666_666_000_000_000_000_000;
+
+        uint256 startBalanceX = tokenX.balanceOf(user1);
+        uint256 startBalanceY = tokenY.balanceOf(user1);
+        uint256 startBalanceZ = market.balanceOf(user1);
+
+        vm.startPrank(user1);
+        tokenX.approve(address(market), x);
+        tokenY.approve(address(market), y);
+
+        vm.expectEmit(true, true, true, true);
+        emit Minted(user1, expectedX, expectedY, expectedZ);
+
+        uint256 z = market.mint(x, y);
+        vm.stopPrank();
+
+        // The tokens should have been subtracted. Liquidity tokens should have
+        // been added.
+        assertEq(startBalanceX - tokenX.balanceOf(user1), expectedX);
+        assertEq(startBalanceY - tokenY.balanceOf(user1), expectedY);
+        assertEq(market.balanceOf(user1) - startBalanceZ, expectedZ);
 
         // The liquidity tokens issued should be proportional to the tokens
         // supplied.
@@ -228,9 +356,67 @@ contract DepositedStateTest is DepositedState {
     function testBurn() public {
         uint256 _balance = market.balanceOf(user1);
         vm.prank(user1);
+
+        vm.expectEmit(true, true, true, true);
+        emit Burned(user1, depositedX, depositedY, _balance);
         (uint256 x, uint256 y) = market.burn(_balance);
 
         assertEq(x, depositedX);
         assertEq(y, depositedY);
+    }
+}
+
+contract MintedStateTest is MintedState {
+    /// @dev Here, test selling in a specific case. Here we know the rounding
+    ///     to test.
+    function testSellX() public {
+        // The current supply is x ~ y : 1 ~ 2/3.
+        // Therefore, selling `x` tokenX yields `y` tokenY:
+        uint256 x = 400;
+        uint256 y = 266;
+
+        tokenX.mint(user2, x);
+
+        uint256 k = computeK();
+
+        vm.startPrank(user2);
+        tokenX.approve(address(market), x);
+        vm.expectEmit(true, true, true, true);
+        emit SoldX(user2, x, y);
+        uint256 yReal = market.sellX(x);
+        vm.stopPrank();
+
+        // k only increases
+        assertEq(computeK(), k + 336572188637229335616);
+
+        // Test that the tokens were transferred correctly.
+        assertEq(tokenX.balanceOf(user2), 0);
+        assertEq(tokenY.balanceOf(user2), y);
+        // Test the return parameter.
+        assertEq(yReal, y);
+    }
+
+    /// @dev Test whether selling doesn't give the user too much. Due to
+    ///     rounding errors exact comparison is not really possible.
+    function testSellY() public {
+        uint256 y = 470;
+        uint256 x = 705;
+
+        tokenY.mint(user2, y);
+
+        uint256 k = computeK();
+
+        vm.startPrank(user2);
+        tokenY.approve(address(market), y);
+        vm.expectEmit(true, true, true, true);
+        emit SoldY(user2, x, y);
+        uint256 xReal = market.sellY(y);
+        vm.stopPrank();
+
+        // k only increases
+        assertEq(computeK(), k + 236952380);
+        assertEq(tokenX.balanceOf(user2), x);
+        assertEq(tokenY.balanceOf(user2), 0);
+        assertEq(xReal, x);
     }
 }
